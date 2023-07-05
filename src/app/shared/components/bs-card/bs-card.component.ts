a266673345cb2bd37817ad1bc168d1471638e495
Mike <mlugo2277@gmail.com>
2023-07-05 14:36:58 -0400

    Build prod

--- src/app/shared/components/bs-card/bs-card.component.ts
+++ src/app/shared/components/bs-card/bs-card.component.ts
@@ -3,32 +3,20 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-bs-card',
   templateUrl: './bs-card.component.html',
-  styleUrls: ['./bs-card.component.css']
+  styleUrls: ['./bs-card.component.css'],
 })
-
 export class BsCardComponent implements OnInit {
-
-  @Input() nombreCharacter:any;
-  @Input() imagenCharacter:any;
-  @Input() estadoCharacter:any;
+  @Input() nombreCharacter: any;
+  @Input() imagenCharacter: any;
+  @Input() estadoCharacter: any;
   estado: any;
   clickable: Boolean = false;
- /*  @Output() newItemEvent = new EventEmitter<string>(); */
 
-ngOnInit(): void {
+  ngOnInit(): void {
     this.inicializarVariables();
-    //this.nombrePersonaje = this.nombreCharacter;
-}
+  }
 
-/* addNewItem(value:any):void{
-  console.log("funcion emiter -->");
-  console.log("valor value", value);
-  this.newItemEvent.emit(value);
-}
- */
-
-inicializarVariables(){
-  this.clickable = false;
-}
-
+  inicializarVariables() {
+    this.clickable = false;
+  }
 }
